84c793582069b40b651d7449f3651d2ad37c9fac
lelimat <lelimaufc@gmail.com>
2015-11-12 12:58:41 -0800

    Now simulating regions for four different types of translocations.

--- simulate_SV_BED.py
+++ simulate_SV_BED.py
@@ -1,14 +1,17 @@
 # simulate_SV_BED.py
 # C: Oct  1, 2015
-# M: Nov  6, 2015
+# M: Nov 12, 2015
 # A: Leandro Lima <leandrol@usc.edu>
 
 
 import sys, argparse
-from random import randint
+from random import randint, choice
 from operator import itemgetter
 
 prog_name = 'simulate_SV_BED.py'
+
+all_chroms = map(str, range(1,23)) + ['X', 'Y']
+
 
 
 def between(start, end, value):
@@ -18,85 +21,78 @@ def between(start, end, value):
         return False
 
 
+
+def pick_a_region(size, start, regions_to_avoid, distance):
+    avoid_start = regions_to_avoid[0][0]
+    avoid_end   = regions_to_avoid[0][1]
+
+    end = start + size - 1
+
+    while between(avoid_start, avoid_end, start) or between(avoid_start, avoid_end, end):
+        start = avoid_end + int(1.1 * distance)
+        end = start + size - 1
+        avoid_start = regions_to_avoid[0][0]
+        avoid_end   = regions_to_avoid[0][1]
+        regions_to_avoid = regions_to_avoid[1:]
+    
+    return start, end, regions_to_avoid
+
+
+
+def parse_chrom_ranges(text):
+    chroms = []
+    chroms_splitted = text.replace('chr', '').split(',')
+    if chroms_splitted == ['all']:
+        chroms = all_chroms
+    else:
+        for piece in chroms_splitted:
+            piece = piece.replace('X', '23')
+            piece = piece.replace('Y', '24')
+            try:
+                first, last = piece.split('-')
+                for c in range(int(first), int(last)+1):
+                    chroms.append(str(c))
+            except:
+                chroms.append(piece)
+    if '23' in chroms:
+        chroms.remove('23')
+        chroms.append('X')
+    if '24' in chroms:
+        chroms.remove('24')
+        chroms.append('Y')
+    return list(set(chroms))
+
+
+
 def main():
 
     parser = argparse.ArgumentParser(description='Get arguments to create random structural variant regions in a BED file.', prog=prog_name)
-    parser.add_argument('--del_lens',   nargs='?', type=file, metavar='del_lengths_file',   dest='del_len_filename',   help='Text file with deletion lengths.')
-    parser.add_argument('--dup_lens',   nargs='?', type=file, metavar='dup_lengths_file',   dest='dup_len_filename',   help='Text file with duplication lengths.')
-    parser.add_argument('--inv_lens',   nargs='?', type=file, metavar='inv_lengths_file',   dest='inv_len_filename',   help='Text file with inversion lengths.')
-    parser.add_argument('--trans_lens', nargs='?', type=file, metavar='trans_lengths_file', dest='trans_len_filename', help='Text file with translocation lengths.')
+    parser.add_argument('--del_lens', required=False, type=file, metavar='del_lengths_file', dest='del_len_filename',   help='Text file with deletion lengths.')
+    parser.add_argument('--dup_lens', required=False, type=file, metavar='dup_lengths_file', dest='dup_len_filename',   help='Text file with duplication lengths.')
+    parser.add_argument('--inv_lens', required=False, type=file, metavar='inv_lengths_file', dest='inv_len_filename',   help='Text file with inversion lengths.')
+    parser.add_argument('--bal_trans_lens', required=False, type=file, metavar='bal_trans_lengths_file', dest='bal_trans_len_filename', help='Text file with balanced translocation lengths.')
+    parser.add_argument('--unb_trans_lens', required=False, type=file, metavar='unb_trans_lengths_file', dest='unb_trans_len_filename', help='Text file with unbalanced translocation lengths.')
     
     parser.add_argument('--chrom_lens', required=True, type=file, metavar='chrom_lengths_file', dest='chrom_lens_file', help='Text file with chromosome lengths.')
     parser.add_argument('--output', '-o', required=True, type=argparse.FileType('w'), metavar='output_bed_file', dest='output_bed_file', help='BED output file.')
     parser.add_argument('--gaps', required=True, type=file, metavar='gaps_file', dest='avoid_regions_file', help='BED file with regions to avoid (centromeres and telomeres).')
-    parser.add_argument('--chrom', required=True, type=str, metavar='chromosome_name', dest='chromosome_name', help='Chromosome.')
+    parser.add_argument('--chroms', required=True, type=str, metavar='chromosome_name', dest='chromosome_name', help='Chromosome.', default='all')
     parser.add_argument('--distance', '-d', type=int, metavar='distance_between_SVs', dest='distance_between_SVs', help='Distance between SVs.', default=100000)
 
     parser.add_argument('-v', '--verbose', action='store_true')
 
     args = parser.parse_args()
 
+
     # Checking if at least one type of variant was provided
-    if args.del_len_filename is None and args.dup_len_filename is None and args.inv_len_filename is None and args.trans_len_filename is None:
+    if args.del_len_filename is None and \
+       args.dup_len_filename is None and \
+       args.inv_len_filename is None and \
+       args.bal_trans_len_filename is None and \
+       args.unb_trans_len_filename is None:
         print '\n\n\tError! At least one file with variant (deletion, duplication, inversion or translocation) lengths has to be provided.\n\n'
         sys.exit(1)
 
-
-    # Example:
-    #       python simulate_SV_BED.py --del_lens SV_lengths.txt --dup_lens SV_lengths.txt -o simulated_SVs_chr7.bed --chrom chr7 --chrom_lens reference/chrom_lengths_hg19.txt --gaps reference/gaps_hg19.txt
-    #
-    # Example for all chromosomes:
-    #       mkdir simulated_SVs;
-    #       for chrom in {1..22} X Y; do python simulate_SV_BED.py --del_lens SV_lengths.txt --dup_lens SV_lengths.txt -o simulated_SVs/simulated_SVs_chr$chrom.bed --chrom chr$chrom --chrom_lens reference/chrom_lengths_hg19.txt --gaps reference/gaps_hg19.txt; done
-
-    if args.chromosome_name.startswith('chr'):
-        args.chromosome_name = args.chromosome_name[3:]
-
-    SV_lens = []
-
-    if not args.del_len_filename is None:
-        lines = args.del_len_filename.read().split('\n')
-        while lines[-1] == '':
-            lines.pop()
-        SV_lens = ['del_'+line for line in lines]
-
-    if not args.dup_len_filename is None:
-        lines = args.dup_len_filename.read().split('\n')
-        while lines[-1] == '':
-            lines.pop()
-        SV_lens += ['dup_'+line for line in lines]
-
-    if not args.inv_len_filename is None:
-        lines = args.inv_len_filename.read().split('\n')
-        while lines[-1] == '':
-            lines.pop()
-        SV_lens += ['inv_'+line for line in lines]
-
-    if not args.trans_len_filename is None:
-        lines = args.trans_len_filename.read().split('\n')
-        while lines[-1] == '':
-            lines.pop()
-        SV_lens += ['trans_'+line for line in lines]
-
-    # Avoiding gap regions (centromeres, telomeres, etc.): https://www.biostars.org/p/2349/#2351
-
-    lines = args.avoid_regions_file.read().split('\n')
-    while lines[-1] == '':
-        lines.pop()
-
-    while lines[0].startswith('#'):
-        lines = lines[1:]
-
-    regions_to_avoid = []
-    for line in lines:
-        chrom, start, end = line.split()[1:4]
-        if chrom.replace('chr', '') == args.chromosome_name:
-            regions_to_avoid.append([int(start), int(end)])
-
-    regions_to_avoid = sorted(regions_to_avoid, key=itemgetter(0), reverse=False)
-    avoid_start = regions_to_avoid[0][0]
-    avoid_end   = regions_to_avoid[0][1]
-    regions_to_avoid = regions_to_avoid[1:]
 
     chrom_lens = {}
     with args.chrom_lens_file as chrom_lens_file_lines:
@@ -108,29 +104,148 @@ def main():
                 pass
 
 
-    chrom_len = chrom_lens[args.chromosome_name]
+    # Avoiding gap regions (centromeres, telomeres, etc.): https://www.biostars.org/p/2349/#2351
+    lines = args.avoid_regions_file.read().split('\n')
+    while lines[-1] == '':
+        lines.pop()
 
-    start = avoid_end + args.distance_between_SVs
-    while SV_lens != []:
-        SV = SV_lens[randint(0, len(SV_lens)-1)]
-        sv_type, sv_size = SV.split('_')
-        end = start + int(sv_size)
-        if end > chrom_len:
-            print 'Impossible to finish process of creating SVs. Not enough chromosome length (size of %s is %d) for simulated SVs.' % (args.chromosome_name, chrom_len)
-            print 'Exiting.'
-            break
-        
-        if between(start, end, avoid_start-args.distance_between_SVs) or between(avoid_start-args.distance_between_SVs, avoid_end+args.distance_between_SVs, start):
-            start = avoid_end + int(1.1*args.distance_between_SVs)
-            avoid_start = regions_to_avoid[0][0]
-            avoid_end   = regions_to_avoid[0][1]
-            regions_to_avoid = regions_to_avoid[1:]
+    while lines[0].startswith('#'):
+        lines = lines[1:]
+
+    regions_to_avoid = {}
+    for line in lines:
+        chrom, start, end = line.split()[1:4]
+        chrom = chrom.replace('chr', '')
+        if not chrom in regions_to_avoid.keys():
+            regions_to_avoid[chrom] = [[int(start), int(end)]]
         else:
-            args.output_bed_file.write('%s\t%d\t%d\t%s\n' % (args.chromosome_name, start, end, sv_type))
-            if args.verbose:
-                print 'Created ', args.chromosome_name, str(start), str(end), sv_type
-            SV_lens.remove(SV)
-            start = end + args.distance_between_SVs
+            regions_to_avoid[chrom] += [[int(start), int(end)]]
+
+    starts = {}
+    ends   = {}
+    for chrom in regions_to_avoid.keys():
+        regions_to_avoid[chrom] = sorted(regions_to_avoid[chrom], key=itemgetter(0), reverse=False)
+        starts[chrom] = 0
+        ends[chrom] = 0
+
+
+    # Parse chromosome ranges to get individual chromosomes
+    chroms = parse_chrom_ranges(args.chromosome_name)
+
+
+    SV_codes = {}
+    SV_regions = {}
+    del_lens = []
+    dup_lens = []
+    inv_lens = []
+    bal_trans_lens = []
+    unb_trans_lens = []
+
+    for chrom in parse_chrom_ranges('all'):
+        SV_regions[chrom] = []
+
+    
+    for chromA in chroms:
+        if args.verbose:
+            print 'Creating SVs for chromosome', chromA
+        SV_codes[chromA] = []
+
+        if not args.del_len_filename is None:
+            if del_lens == []:
+                lines = args.del_len_filename.read().split('\n')
+                while lines[-1] == '':
+                    lines.pop()
+                del_lens = lines
+            SV_codes[chromA] += ['del_'+length for length in del_lens]
+
+        if not args.dup_len_filename is None:
+            if dup_lens == []:
+                lines = args.dup_len_filename.read().split('\n')
+                while lines[-1] == '':
+                    lines.pop()
+                dup_lens = lines
+            SV_codes[chromA] += ['dup_'+length for length in dup_lens]
+
+        if not args.inv_len_filename is None:
+            if inv_lens == []:
+                lines = args.inv_len_filename.read().split('\n')
+                while lines[-1] == '':
+                    lines.pop()
+                inv_lens = lines
+            SV_codes[chromA] += ['inv_'+length for length in inv_lens]
+
+        if not args.bal_trans_len_filename is None:
+            if bal_trans_lens == []:
+                lines = args.bal_trans_len_filename.read().split('\n')
+                while lines[-1] == '':
+                    lines.pop()
+                bal_trans_lens = lines
+            for length in bal_trans_lens:
+                chromB = choice(all_chroms)
+                SV_codes[chromA] += ['baltr_'+length+'_'+chromB]
+
+        if not args.unb_trans_len_filename is None:
+            if unb_trans_lens == []:
+                lines = args.unb_trans_len_filename.read().split('\n')
+                while lines[-1] == '':
+                    lines.pop()
+                unb_trans_lens = lines
+            for length in unb_trans_lens:
+                chromB = choice(all_chroms)
+                SV_codes[chromA] += ['unbtr_'+length+'_'+chromB]
+
+
+        chromA_len = chrom_lens[chromA]
+
+        endA = 1
+
+        while SV_codes[chromA] != []:
+            chromB = None
+            SV = choice(SV_codes[chromA])
+            try:
+                sv_type, sv_size = SV.split('_')
+            except:
+                sv_type, sv_size, chromB = SV.split('_')
+
+            sv_size = int(sv_size)
+
+            startA, endA, regions_to_avoid[chromA] = pick_a_region(sv_size, endA+args.distance_between_SVs, regions_to_avoid[chromA], args.distance_between_SVs)
+            if endA > chromA_len:
+                print '\n\n\tImpossible to finish process of creating SVs for chrom %s. Not enough chromosome length (size is %d) for simulated SVs.' % (chromA, chromA_len)
+                print '\tExiting.\n\n'
+                break
+            
+            if chromB is None:
+                SV_regions[chromA] += [sv_type + '_' + str(startA) + '_' + str(endA)]
+            else:
+                if SV_regions[chromB] == []:
+                    # If no SV was inserted in chromB, start from the first position in chromosome
+                    startB, endB, regions_to_avoid[chromB] = pick_a_region(sv_size, 1, regions_to_avoid[chromB], args.distance_between_SVs)
+                else:
+                    # Else, get the last SV inserted and start from the end of such SV
+                    sv_typeB, startB, endB = SV_regions[chromB][-1].split('_')[:3]
+                    startB, endB, regions_to_avoid[chromB] = pick_a_region(sv_size, int(endB) + args.distance_between_SVs, regions_to_avoid[chromB], args.distance_between_SVs)
+                SV_regions[chromA] += [sv_type + '_' + str(startA) + '_' + str(endA) + '_' + chromB + ':' + str(startB) + '-' + str(endB)]
+                if sv_type == 'unbtr':
+                    SV_regions[chromB] += ['unaff_' + str(startB) + '_' + str(endB) + '_' + chromA + ':' + str(startA) + '-' + str(endA)]
+                else:
+                    SV_regions[chromB] += [sv_type + '_' + str(startB) + '_' + str(endB) + '_' + chromA + ':' + str(startA) + '-' + str(endA)]
+
+
+            SV_codes[chromA].remove(SV)
+
+    for chrom in all_chroms:
+        for SV in SV_regions[chrom]:
+            elems = SV.split('_')
+            sv_type = elems[0]
+            if sv_type != 'unaff': # This means that the region in the current chromosome was not affected
+                start   = elems[1]
+                end     = elems[2]
+                if len(elems) > 3:
+                    info = elems[3]
+                    args.output_bed_file.write('%s\t%s\t%s\t%s\t%s\n' % (chrom, start, end, sv_type, info))
+                else:
+                    args.output_bed_file.write('%s\t%s\t%s\t%s\n' % (chrom, start, end, sv_type))
 
 
 if __name__ == '__main__':

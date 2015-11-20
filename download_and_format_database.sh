15c317c44d2deb7e956ad09021e4b6b0698bcea6
lelimat <lelimaufc@gmail.com>
2015-11-19 16:08:25 -0800

    Updated with spliting of frequency files.

--- download_and_format_database.sh
+++ download_and_format_database.sh
@@ -1,6 +1,6 @@
 # download_and_format_database.sh
 # C: Oct 23, 2015
-# M: Oct 28, 2015
+# M: Nov 19, 2015
 # A: Leandro Lima <leandrol@usc.edu>
 
 if [ "$#" -ne 1 ]; then
@@ -25,18 +25,19 @@ echo "Downloading frequencies of variants for" $gv.
 wget http://www.openbioinformatics.org/annovar/download/$zip_file
 
 unzip $zip_file
-rm $zip_file
+# mv $gv""_1000g2015aug/* .
+rm $zip_file #$gv""_1000g2015aug
 
-# # Separating chromosomes in variant files
-# for pop in AFR ALL AMR EAS EUR SAS; do
-#     echo "Filtering variants for" $pop "population."
-#     echo -n "Chromosomes: "
-#     for chrom in {1..22} X Y; do
-#         echo -n $chrom" "
-#         grep -w ^$chrom $gv""_$pop.sites.2015_08.txt > $gv""_$pop.sites.2015_08.chrom$chrom.txt
-#     done
-#     echo -e "\nDone.\n"
-# done
+# Separating chromosomes in variant files
+for pop in AFR ALL AMR EAS EUR SAS; do
+    echo "Filtering variants for" $pop "population."
+    echo -n "Chromosomes: "
+    for chrom in {1..22} X Y; do
+        echo -n $chrom" "
+        grep -w ^$chrom $gv""_$pop.sites.2015_08.txt > $gv""_$pop.sites.2015_08.chrom$chrom.txt
+    done
+    echo -e "\nDone.\n"
+done
 
 
 # Download fasta references
@@ -55,5 +56,10 @@ if [ "$gv" == "hg38" ]; then
     mv hg38/chroms/* hg38/
     rmdir hg38/chroms
 fi
+
+cd $gv
+for chrom in {1..22} X Y; do
+    echo "bwa index chr$chrom.fa" | qsub -cwd -V -N chr$chrom""_index
+done
 
 cd ..
